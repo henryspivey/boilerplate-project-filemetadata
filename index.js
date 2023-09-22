adf3ec022b0c09c76db28fee6b6a490400b3a2fb
Henry Spivey <henry.spivey@changehealthcare.com>
2023-09-22 15:05:32 -0700

    remove uploads folder

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -11,7 +13,10 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  const {originalname, mimetype, size} = req.file
+  res.json({originalname, mimetype, size})
+})
 
 
 const port = process.env.PORT || 3000;
